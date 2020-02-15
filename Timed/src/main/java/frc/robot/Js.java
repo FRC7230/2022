a6aff4447a0bc4c63f91ac11be376243d24e7468
Eric <36435265+EricXu7230@users.noreply.github.com>
2020-02-15 11:37:51 -0800

    Removed JS and Intake
    we dont need them so i removed them

--- Timed/src/main/java/frc/robot/Js.java
+++
@@ -1,8 +0,0 @@
-package frc.robot;
-import edu.wpi.first.wpilibj.Joystick;
-public class Js{
-    private final Joystick m_stick = new Joystick(0);
-    public Joystick getJs(){
-        return m_stick;
-    }
-}
